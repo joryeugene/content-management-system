d4513925c1e8a503c21d9bc8d76832b7b90c0c52
joryeugene <jorypestorious@outlook.com>
2016-11-18 13:49:18 -0600

    delete some testing/unneeded stuff from blogMainPage.js

--- CMS/src/main/webapp/js/blogMainPage.js
+++ CMS/src/main/webapp/js/blogMainPage.js
@@ -7,17 +7,13 @@ function loadMainPagePosts() {
         type: 'GET',
         url: "/CMS/posts/recent"
     }).success(function (data, status) {
-        console.log(data);
         populateMainPagePost(data);
-
     });
 }
 
 function populateMainPagePost(data) {
     var mainPagePostsDiv = $('#main-page-posts');
     mainPagePostsDiv.empty();
-
-    // need to reverse order and limit to 6 and add a button at bottom of page for MORE posts
 
     $.each(data, function (index, post) {
         if (post.content.length > 231) {
@@ -43,10 +39,6 @@ function populateMainPagePost(data) {
                 ) // ends the 2nd <p>
                 ); // ends the <div> 
 
-
-
-
-
         if ((index + 1) % 3 === 0) {
             mainPagePostsDiv.append($('<div class="clearfix visible-lg-block"></div>'));
         }
@@ -55,42 +47,3 @@ function populateMainPagePost(data) {
         }
     });
 }
-
-//
-//.append($('<p id="edit-delete-container">')
-//                .append($('<a>').attr({
-//                    'onClick': 'deleteDVD(' + dvd.id + ')'
-//        })
-//                .append('<span class="glyphicon glyphicon-remove" id="delete-btn" aria-hidden="true"></span><br>')
-//                ) // end <a>
-//                .append($('<a>').attr({
-//                    'data-dvd-id': dvd.id,
-//            'data-toggle': 'modal',
-//            'data-target': '#edit-modal'
-//        })
-//                .append('<span class="glyphicon glyphicon-cog" id="edit-btn" aria-hidden="true"></span>')
-//                ) // end <a>
-//                ) // end <p>
-//                .append($('<a style="cursor:pointer;">')
-//                .attr({
-//                    'data-dvd-id': dvd.id,
-//            'data-toggle': 'modal',
-//            'data-target': '#details-modal'
-//        }).append($('<div>')
-//                .attr({
-//                    'class': 'dvd-inner-tile'
-//        })
-//                .append($('<img>')
-//                .attr({
-//                    'class': 'dvd-image img-responsive',
-//            'id': imgDiv,
-//            'src': dvd.image
-//        })
-//                ) // ends the <img>
-//                )// ends the <div> 
-//                .append($('<p>').attr({
-//                    'class': 'dvd-title'
-//        })
-//                .text(dvd.title)
-//                ) // ends <p>
-//                ) // ends the <a> tag

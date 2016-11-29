6bcb3195ceb399ffbfe2af0b98424f253ab80ecd
Sarita Salm <comsalm2@gmail.com>
2016-11-28 21:16:13 -0600

    security related changes, still need to deal with enabled for user

--- CMS/src/main/java/com/teamexcalibur/controller/LoginController.java
+++ CMS/src/main/java/com/teamexcalibur/controller/LoginController.java
@@ -1,8 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.teamexcalibur.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
+    // #1 - respond to all GET requests for /login
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginForm() {
+        return "login";
+    }
 }
